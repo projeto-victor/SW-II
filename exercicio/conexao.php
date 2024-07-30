99f44bcc514680cbe51efb4199f77fa19b75438f
Victor Marques <100208584+projeto-victor@users.noreply.github.com>
2024-07-30 08:21:37 -0300

    Add files via upload

--- exercicio/conexao.php
+++ exercicio/conexao.php
@@ -1,16 +1,15 @@
 <?php
-    $host = 'localhost';
+    $host = 'localhost';    
     $usuario = 'root';
     $senha = '';
     $banco = 'etecmcm';
 
-    $conexao = new mysqli($host, $usuario, $senha< $banco);
+    $conexao = new mysqli($host,$usuario,$senha,$banco);
 
     if($conexao->connect_error){
-        die('Falha de conexão: ' .conexao->connect_error);
+        die('Falha de conexão: ' . $conexao->connect_error);
     }
-
-//   else{
-//       echo "CONECTADO COM SUCESSO";
-//    }
+    //else{
+    //     echo "CONECTADO COM SUCESSO";
+    //}
 ?>
